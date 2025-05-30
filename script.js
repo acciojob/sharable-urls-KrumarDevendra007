ed40cad874407bfa5ee9b033412c026a6b1e500a
Kumar Devendra <67205183+KrumarDevendra007@users.noreply.github.com>
2025-05-30 14:48:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// your code here
+// your code here.
+
+
+
+const updateUrl = () => {
+
+const urlInput = document.getElementById("url");
+const nameInput = document.getElementById("name").value;
+const yearInput = document.getElementById("year").value;
+	
+urlInput.innerText = `https://localhost:8080/?name=${nameInput}&year=${yearInput}`
+}
